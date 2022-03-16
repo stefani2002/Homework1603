a16f15a9039dbc0b113d9dabdbaf840e78ee6b84
stefani2002 <98656153+stefani2002@users.noreply.github.com>
2022-03-16 19:42:50 +0100

    Add files via upload

---
+++ HomeworkTask1/HomeworkTask1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeworkTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============SumOfEven==========");
+
+            int sum = 0;
+            int[] arrayOfIntegers = new int[5];
+
+            for (int i = 0; i < arrayOfIntegers.Length; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                arrayOfIntegers[i] = int.Parse(Console.ReadLine());
+
+                if (arrayOfIntegers[i] % 2 == 0)
+                {
+                    sum += arrayOfIntegers[i];
+                }
+                
+            }
+
+            Console.WriteLine("The sum of even numbers is:");
+            Console.WriteLine(sum);
+
+
+           Console.ReadLine();
+
+        }
+    }
+}
---
+++ HomeworkTask2/HomeworkTask2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HomeworkTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========Student Group========");
+            Console.WriteLine(" ");
+
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Stefani", "Jana", "Maja", "Petar", "Angel" };
+
+            for (; ; )
+            {
+                Console.WriteLine("Input a number:");
+                bool success = int.TryParse(Console.ReadLine(), out int number);
+                if (success)
+                {
+                    if (number == 1)
+                    {
+                        Console.WriteLine("The students in G1 are:");
+                        for (int i = 0; i < studentsG1.Length; i++)
+                        {
+                            Console.WriteLine(studentsG1[i]);
+                        }
+                        break;
+                    }
+                    else if (number == 2)
+                    {
+                        Console.WriteLine("The students in G2 are:");
+                        for (int i = 0; i < studentsG2.Length; i++)
+                        {
+                            Console.WriteLine(studentsG2[i]);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please insert a number between 1 or 2:");
+                        continue;
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input!");
+                }
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
